36e4924c6309273cd34e9a14c6cfc124ca51a65b
Evgeny Terentyev <eterente94@gmail.com>
2016-01-07 15:14:24 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+/*
+Trains:
+Приложение, моделирующее информационную систему на железнодорожном вокзале
+
+Main:
+Точка входа в приложение
+
+Терентьев Евгений, ИВТ-42БО
+ */
+
+import javax.swing.*;
+
+public class Main {
+    /**
+     * Точка входа в приложение
+     * @param args аргументы
+     */
+    public static void main(final String[] args) {
+        JFrame mainFrame = new JFrame("Payments");
+       // mainFrame.setContentPane(new TrainsController().getPanel());
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        mainFrame.pack();
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setVisible(true);
+    }
+}
