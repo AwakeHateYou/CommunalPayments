3b8d3171b728c874d4df1ad8140ba247054f211a
Evgeny Terentyev <eterente94@gmail.com>
2016-01-08 14:53:41 +0300

    windows half done

--- src/main/java/ui/PaymentsController.java
+++ src/main/java/ui/PaymentsController.java
@@ -1,4 +1,7 @@
 package ui;
+import model.PaymentEntity;
+import store.StorePayments;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,15 +10,28 @@ import java.awt.*;
  */
 public class PaymentsController extends JPanel {
     private JScrollPane scrollPayments;
-    private JList<String> listPayments;
+    private JList<PaymentEntity> listPayments;
+    private DefaultListModel<PaymentEntity> paymentsListModel;
     public PaymentsController(){
         initialize();
         add(scrollPayments);
         setPreferredSize(new Dimension(300,300));
-       // setSize(300, 300);
+        updateListWithItems(listPayments, StorePayments.allObjectWithClass(PaymentEntity.class));
     }
     private void initialize(){
         scrollPayments = new JScrollPane();
         scrollPayments.add(new JLabel("Test"));
+    }
+    /**
+     * Добавляет объекты в список
+     * @param listItems список
+     * @param items объекты
+     */
+    private void updateListWithItems(JList<PaymentEntity> listItems, java.util.List<PaymentEntity> items) {
+        DefaultListModel<PaymentEntity> model = new DefaultListModel<>();
+        items.forEach(model::addElement);
+
+        paymentsListModel = model;
+        listItems.setModel(model);
     }
 }
