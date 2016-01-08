b08ff65392cf32e8ef80a0b02de2fc7e1c949155
Andrey Martynov <andremartynov@gmail.com>
2016-01-08 15:36:54 +0300

    PaymentController refactoring

--- src/main/java/ui/CreatePaymentController.java
+++ src/main/java/ui/CreatePaymentController.java
@@ -6,45 +6,67 @@ import java.awt.*;
  * Окно создания счета.
  */
 public class CreatePaymentController extends JFrame{
-    private JTextField fio, price, priceDone;
-    private JButton accept, cancel;
-    private JComboBox typeList;
-    private String[] defaultPayType = {"Квартплата", "Плата за электроэнергию", "Плата за телефон"};
-    public CreatePaymentController(){
-        initialize();
-        setLayout(new FlowLayout());
+
+    public CreatePaymentController() {
+        getContentPane().setLayout(new GridLayout(5, 1));
+        initComponents();
         setPreferredSize(new Dimension(350, 280));
         setResizable(false);
-
     }
-    private void initialize(){
-        fio = new JTextField();
-        price = new JTextField();
-        priceDone = new JTextField();
-        typeList = new JComboBox(defaultPayType);
-        accept = new JButton("Принять");
-        cancel = new JButton("Отменить");
-        fio.setPreferredSize(new Dimension(200, 24));
-        price.setPreferredSize(new Dimension(200, 24));
-        priceDone.setPreferredSize(new Dimension(200, 24));
 
+    private void initComponents() {
+        setTitle("Добавление платежа");
+        setFioLayout();
+        setPriceLayout();
+        setPriceDefaultLayout();
+        setTypeLayout();
+        setButtonLayout();
+        pack();
+    }
+
+    private void setFioLayout() {
         JPanel fioPanel = new JPanel();
-        JPanel pricePanel = new JPanel();
-        JPanel priceDonePanel = new JPanel();
-        JPanel typePanel = new JPanel();
+        JTextField fio = new JTextField();
+        fio.setPreferredSize(new Dimension(200, 24));
         fioPanel.add(new JLabel("Ф.И.О."));
         fioPanel.add(fio);
+        getContentPane().add(fioPanel);
+    }
+
+    private void setPriceLayout() {
+        JPanel pricePanel = new JPanel();
+        JTextField price = new JTextField();
+        price.setPreferredSize(new Dimension(200, 24));
         pricePanel.add(new JLabel("Сумма платежа"));
         pricePanel.add(price);
+        getContentPane().add(pricePanel);
+    }
+
+    private void setPriceDefaultLayout() {
+        JPanel priceDonePanel = new JPanel();
+        JTextField priceDone = new JTextField();
+        priceDone.setPreferredSize(new Dimension(200, 24));
         priceDonePanel.add(new JLabel("Внесенная сумма"));
         priceDonePanel.add(priceDone);
+        getContentPane().add(priceDonePanel);
+    }
+
+    private void setTypeLayout() {
+        JPanel typePanel = new JPanel();
+        JComboBox<String> typeList = new JComboBox<>(
+                new String[] {"Квартплата", "Плата за электроэнергию", "Плата за телефон"});
         typePanel.add(new JLabel("Вид платежа"));
         typePanel.add(typeList);
-        add(fioPanel);
-        add(pricePanel);
-        add(priceDonePanel);
-        add(typePanel);
-        add(accept);
-        add(cancel);
+        getContentPane().add(typePanel);
     }
+
+    private void setButtonLayout() {
+        JPanel buttonPanel = new JPanel();
+        JButton accept = new JButton("Принять");
+        JButton cancel = new JButton("Отменить");
+        buttonPanel.add(accept);
+        buttonPanel.add(cancel);
+        getContentPane().add(buttonPanel);
+    }
+
 }
