829b4de6355f796ea1a9683b54947c895d02d9f1
Evgeny Terentyev <eterente94@gmail.com>
2016-01-14 15:30:29 +0300

    debtors half done

--- src/main/java/terentev/evgenyi/ui/CreateDebtorsListController.java
+++ src/main/java/terentev/evgenyi/ui/CreateDebtorsListController.java
@@ -3,9 +3,12 @@ package terentev.evgenyi.ui;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import terentev.evgenyi.store.StorePayments;
+import terentev.evgenyi.util.NotAPositiveValueException;
+import terentev.evgenyi.util.WrongEdgesException;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Arc2D;
 
 public class CreateDebtorsListController extends JFrame{
     private JList<Object> listDebtors;
@@ -22,6 +25,7 @@ public class CreateDebtorsListController extends JFrame{
         initSpinnersPane();
         initButtonPane();
         findDebtors();
+        bind();
     }
     private void initScrollPane(){
         listDebtors = new JList<>();
@@ -31,11 +35,16 @@ public class CreateDebtorsListController extends JFrame{
     private void initSpinnersPane(){
         from = new JSpinner();
         from.setValue(1);
+        from.setEnabled(false);
+        from.setPreferredSize(new Dimension(75, 24));
         to = new JSpinner();
+        to.setPreferredSize(new Dimension(75, 24));
         to.setValue(2);
+        to.setEnabled(false);
         inRange = new JRadioButton();
         inRange.setSelected(false);
         JPanel spinners = new JPanel();
+        spinners.add(new JLabel("Выбор с размером долга "));
         spinners.add(inRange);
         spinners.add(from);
         spinners.add(to);
@@ -45,25 +54,60 @@ public class CreateDebtorsListController extends JFrame{
         accept = new JButton("Приянть");
         getContentPane().add(accept);
     }
+    private void bind(){
+        accept.addActionListener(e -> findDebtors());
+        inRange.addActionListener(e -> activateSpinners());
+    }
     /**
      * Поиск всех долждников
      */
     private void findDebtors() {
-        Session session = StorePayments.getSession();
+        try {
+            checkFieldCorrect();
+            Session session = StorePayments.getSession();
+            String queryString = "select fio from PaymentEntity payment where payment.price > payment.priceDone";
+            if (inRange.isSelected()) {
+                queryString += " and payment.price - payment.priceDone > " + from.getValue() + " and payment.price - payment.priceDone < " + to.getValue();
+            } else {
+                queryString += " order by payment.fio";
+            }
+            Query query = session.createQuery(queryString);
+//        query.setParameter("lowEdge", from.getValue());
+//        query.setParameter("upEdge", to.getValue());
+            listDebtors.setListData(query.list().toArray());
 
-        String queryString = "select fio from PaymentEntity payment where payment.price > payment.priceDone";
-        if (inRange.isSelected()) {
-            queryString += " and payment.priceDone > :ID";
-        } else {
-             queryString += " order by payment.fio";
+            session.close();
+        }catch (Exception e){
+            catchException(e);
         }
 
-        Query query = session.createQuery(queryString).setParameter("ID", 10);
-//        query.setParameter("ID", from.getValue());
-//        query.setParameter("upEdge", Double.parseDouble(to.getValue().toString()));
-        listDebtors.setListData(query.list().toArray());
+    }
 
-        session.close();
+    /**
+     * Включение и выключение полей с размером долга.
+     */
+    private void activateSpinners(){
+        if(inRange.isSelected()) {
+            from.setEnabled(true);
+            to.setEnabled(true);
+        }else{
+            from.setEnabled(false);
+            to.setEnabled(false);
+        }
+    }
+    private void checkFieldCorrect() throws Exception{
+        if(Double.parseDouble(from.getValue().toString()) < 0 || Double.parseDouble(from.getValue().toString()) <= 0)
+            throw new NotAPositiveValueException();
+        if(Double.parseDouble(from.getValue().toString()) > Double.parseDouble(from.getValue().toString()))
+            throw new WrongEdgesException();
+
+    }
+    /**
+     * Ловит все исключения.
+     * @param e - NumberFormatException, NotAPositiveValueException, WrongAmpuntNeighboursException.
+     */
+    private static void catchException(Exception e) {
+        new JOptionPane().showMessageDialog(null, e.getMessage(), "Alert", JOptionPane.ERROR_MESSAGE);
     }
 
 }
